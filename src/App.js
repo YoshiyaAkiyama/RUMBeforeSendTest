caf19ecf070899c54102e2e396cbfbd61a45ecb5
Yoshiya Akiyama <65460470+YoshiyaAkiyama@users.noreply.github.com>
2023-11-21 17:53:56 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Banner from "./Components/Banner";
+import LandingPage from "./Pages/LandingPage";
+import ProductsDisplay from "./Pages/ProductsDisplay";
+import WishlistPage from "./Pages/WishlistPage";
+import CartPage from "./Pages/CartPage";
+import { DataProvider } from "./Context/DataContext";
+import { CartProvider } from "./Context/CartContext";
+import { WishlistProvider } from "./Context/WishlistContext";
+import { FeedbackProvider } from "./Context/FeedbackContext";
+import Feedback from "./Components/Feedback";
+import { FilterProvider } from "./Context/FilterContext";
+
+// add @datadog/browser-rum in the dependancies
+
+// IMPORTANT ===>> add the RUM init code below and useCrossSiteSessionCookie: true
+
+export default function App() {
+  return (
+    <div className="App">
+      <DataProvider>
+        <Navbar />
+        <Banner />
+        <CartProvider>
+          <WishlistProvider>
+            <FeedbackProvider>
+              <FilterProvider>
+                <Feedback />
+                <Routes>
+                  <Route path="/" element={<LandingPage />} />
+                  <Route
+                    path="/products/:category"
+                    element={<ProductsDisplay />}
+                  />
+                  <Route path="wishlist" element={<WishlistPage />} />
+                  <Route path="cart" element={<CartPage />} />
+                </Routes>
+              </FilterProvider>
+            </FeedbackProvider>
+          </WishlistProvider>
+        </CartProvider>
+      </DataProvider>
+    </div>
+  );
+}
